608d9ff616ef3f858a96c71514ba7139f8d8a64c
paul-tes <pauasca8462@gmail.com>
2023-12-07 17:42:35 +0300

    Add lock icon and input field to login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,6 +7,59 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              // logo
+              Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+
+              SizedBox(height: 50),
+              // welcome back, you've been missed
+              Text(
+                "Welocme to BetLembosa",
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+
+              SizedBox(height: 25),
+              // password text field
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                  )
+                ),
+              ),
+              // forgoten password
+              
+              // sign in butoon
+              
+              // or continue with
+              
+              // google + apple sign in buton
+              
+              // not a menber reqister now!
+            ]
+          ),
+        ),
+      )
+    );
   }
 }
