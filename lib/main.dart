ee7474079831dcd4dd95a9b889171e0718a688ca
paul-tes <pauasca8462@gmail.com>
2023-12-30 04:13:24 +0300

    Add login form and login dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:betlembosa/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,6 +18,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent
+      )
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:betlembosa/components/my_button.dart';
 import 'package:betlembosa/components/my_text_filed.dart';
+import 'package:betlembosa/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -22,97 +23,128 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              // logo
-              Icon(
-                  Icons.lock,
-                  size: 100,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 30),
+                // logo
+                Icon(
+                    Icons.home_rounded,
+                    size: 70,
+                  ),
+        
+                SizedBox(height: 35),
+                // welcome back, you've been missed
+                Text(
+                  "Welocme to BetLembosa",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
                 ),
-
-              SizedBox(height: 50),
-              // welcome back, you've been missed
-              Text(
-                "Welocme to BetLembosa",
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 16,
+        
+                SizedBox(height: 25),
+        
+                // user name text field
+                MyTextField(
+                  controller: userNameController,
+                  hintText: "User Name",
+                  obscureText: false
                 ),
-              ),
-
-              SizedBox(height: 25),
-
-              // user name text field
-              MyTextField(
-                controller: userNameController,
-                hintText: "User Name",
-                obscureText: false
-              ),
-
-              SizedBox(height: 10),
-              // password text field
-              MyTextField(
-                controller: passwordController,
-                hintText: "password",
-                obscureText: true,
-              ),
-
-              SizedBox(height: 10),
-              // forgoten password
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+        
+                SizedBox(height: 10),
+                // password text field
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "password",
+                  obscureText: true,
+                ),
+        
+                SizedBox(height: 10),
+                // forgoten password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password",
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+        
+                SizedBox(height: 25.0),
+                // sign in butoon
+                MyButton(
+                  onTap: signUserIn,
+                ),
+                SizedBox(height: 50.0),
+                // or continue with
+        
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          "Or continue with",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+                
+                SizedBox(height: 50.0),
+        
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Forgot Password",
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
+                    // google + apple sign in buton
+                    SquareTile(imagePath: 'lib/images/google.png'),
+        
+                    SizedBox(width: 10),
+        
+                    SquareTile(imagePath: 'lib/images/apple.png'),
                   ],
                 ),
-              ),
-
-              SizedBox(height: 25.0),
-              // sign in butoon
-              MyButton(
-                onTap: signUserIn,
-              ),
-              SizedBox(height: 50.0),
-              // or continue with
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
+        
+                SizedBox(height: 40.0),
+                // not a menber reqister now!
+        
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        "Or continue with",
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[600],
-                      ),
+                    Text('Not a menber?'),
+                    const SizedBox(width: 4),
+                    Text(
+                      'Register Now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      )
                     ),
                   ]
-                ),
-              ),
-              
-              // google + apple sign in buton
-              
-              // not a menber reqister now!
-            ]
+                )
+              ]
+            ),
           ),
         ),
       )
