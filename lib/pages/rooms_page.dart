4cff826352910e2808aa8fb76665f70185f04afb
paul-tes <paulasca8462@gmail.com>
2024-04-21 23:08:54 +0300

    Add search Bar to room list

--- lib/pages/rooms_page.dart
+++ lib/pages/rooms_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class RoomsPage extends StatelessWidget {
+class RoomsPage extends StatefulWidget {
   const RoomsPage({super.key});
 
   @override
+  State<RoomsPage> createState() => _RoomsPageState();
+}
+
+class _RoomsPageState extends State<RoomsPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("List Of Rooms Page"),
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(12.0),
+          margin: EdgeInsets.symmetric(horizontal: 25.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8)
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
