5b2ea0bcc94e147b22a00e9fb1bfaab5e6e95afb
paul-tes <paulasca8462@gmail.com>
2024-04-21 22:12:07 +0300

    Fix overflow pixel of the bottom buttons by reducing the size

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -7,38 +7,40 @@ class MyBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child:  GNav(
-          color: Colors.grey[400],
-          activeColor: Colors.grey.shade700,
-          tabActiveBorder: Border.all(color: Colors.white),
-          tabBackgroundColor: Colors.grey.shade100,
-          mainAxisAlignment: MainAxisAlignment.center,
-          tabBorderRadius: 16,
-          onTabChange: (value) => onTabChange!(value),
-          tabs: const [
-            GButton(
-              icon: Icons.home,
-              text: "Rooms",
-            ),
-      
-            GButton(
-              icon: Icons.checklist_outlined,
-              text: "Reservations",
-            ),
-      
-            GButton(
-              icon: Icons.calendar_month,
-              text: "Booking",
-            ),
-      
-            GButton(
-              icon: Icons.add_business,
-              text: "My Rooms",
-            )
-          ]
-        ),
+    return Container(
+      child:  GNav(
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: "Rooms",
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          ),
+    
+          GButton(
+            icon: Icons.checklist_outlined,
+            text: "Reservations",
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          ),
+    
+          GButton(
+            icon: Icons.calendar_month,
+            text: "Booking",
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          ),
+    
+          GButton(
+            icon: Icons.add_business,
+            text: "My Rooms",
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          )
+        ]
       ),
     );
   }
